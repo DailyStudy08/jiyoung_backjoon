a3c1f01816b6d2caf41fe7bc75e8914808346105
luminaries <luminaries1@naver.com>
2022-08-12 21:26:56 +0900

    16928

--- class1/11286.py
+++ class1/11286.py
@@ -9,5 +9,14 @@ heap = []
 for i in range(N):
     a = int(input())
     if a == 0:
-        heapq.heappop(heap)
-        
+        if heap:
+            pop_data = heapq.heappop(heap)
+            print(pop_data[1])
+        else:
+            print('0')
+    else:
+        if a<0:
+            heapq.heappush(heap, (abs(a)*2-1,a))
+        else:
+            heapq.heappush(heap, (abs(a)*2,a))
+            
--- class1/16928.py
+++ class1/16928.py
@@ -1,48 +1,46 @@
 import sys
 from collections import deque
-import math
 
 
-# input = sys.stdin.readline
-f = open('input.txt','r')
-input = f.readline
+input = sys.stdin.readline
+# f = open('input.txt','r')
+# input = f.readline
 
-visited = [False]* 101
+visited = [0]* 101
 n, m = map(int, input().split())
-ladder_lst = [list(map(int, input().split())) for _ in range(n)]
-snake_lst = [list(map(int, input().split())) for _ in range(m)]
-graph = [[] for _ in range(101)]
 
-for i in range(1,100):
-    graph[i].append(i+1)
+graph = [0]*101
+for i in range(1,101):
+    graph[i] = i
 
-for coord in ladder_lst:
-    graph[coord[0]].append(coord[1])
+for coord in range(n):
+    a,b = map(int,input().split())
+    graph[a] = b
 
-for coord in snake_lst:
-    graph[coord[0]].append(coord[1])
+for coord in range(m):
+    a,b = map(int,input().split())
+    graph[a]= b
 
 
 def bfs(v):
         q = deque()
-
         q.append(v) 
-        visited[v] = True 
-        cnt = 0
         while q:
             v = q.popleft()
+            for i in range(1,7):
+                next = v + i
+                if next > 100:
+                    continue
+                
+                next = graph[next]
 
-            print(v, end= ' ') 
-            # if v == 100:
-            #     print(int(math.ceil(cnt/6)))
-            #     break
-            adj_v = graph[v] 
-            for u in adj_v: 
-                if not visited[u]:
-                    cnt += 1
-                    q.append(u)
-                    visited[u] = True
+                if visited[next] == 0:
+                    q.append(next)
+                    visited[next] = visited[v] +1
 
+                    if next == 100:
+                        return
 bfs(1)
+print(visited[100])
 
 

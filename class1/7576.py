84f96cca556a4040bf2fe002ffd6953c8992f82d
luminaries <luminaries1@naver.com>
2022-08-23 20:55:15 +0900

    7576

---
+++ class1/7576.py
@@ -0,0 +1,62 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+m, n = map(int, input().split())
+mat = [list(map(int, input().split())) for _ in range(n)]
+adj_lst = [[[] for _ in range(m)] for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
+cnt = [[0]*m for _ in range(n)]
+start = []
+
+
+for i in range(n):
+    for j in range(m):
+        if j+1 < n :
+            adj_lst[i][j].append((i,j+1))
+        if j-1 >=0:
+            adj_lst[i][j].append((i,j-1))
+        if i-1 >= 0:
+            adj_lst[i][j].append((i-1,j))
+        if i+1 < n:
+            adj_lst[i][j].append((i+1,j))
+        
+        if mat[i][j] == 1:
+            visited[i][j] = True
+            start.append((i,j))
+        
+        if mat[i][j] == -1:
+            visited[i][j] = True
+
+def bfs(start):
+    q = deque()
+    for s in start:
+        q.append(s)
+
+    while q:
+        v = q.popleft()
+
+        for i in adj_lst[v[0]][v[1]]:
+            if not visited[i[0]][i[1]]:
+                cnt[i[0]][i[1]] = cnt[v[0]][v[1]] +1
+                visited[i[0]][i[1]] = True
+                q.append(i)
+
+bfs(start)  
+ans = 0
+is_not_rooten = False
+for i in range(n):
+    for j in range(m):
+        if ans < cnt[i][j]:
+            ans = cnt[i][j]
+        
+        if cnt[i][j] == 0 and visited[i][j] == False:
+            ans = -1
+            is_not_rooten = True
+            break
+    if is_not_rooten:
+        break
+
+print(ans)
+
+
+
--- class1/test.py
+++ class1/test.py
@@ -1,71 +1,7 @@
-from heapq import heappush, heappop
-import sys
-input = sys.stdin.readline
+adj_lst = [[[] for x in range(5)] for _ in range(5)]
 
+for i in range(5):
+    for j in range(5):
+        adj_lst[i][j].append(i+j)
 
-def solve():
-    minq = []
-    maxq = []
-    pd = dict()
-    for _ in range(int(input())):
-        cmd = input().split()
-        if cmd[0] == 'I':
-            v=int(cmd[1])
-            if v in pd:
-                pd[v] += 1
-            else:
-                pd[v] = 1
-                if v >= 0:
-                    heappush(maxq, -v)
-                else:
-                    heappush(minq, v)
-        else:
-            if not minq and not maxq:
-                continue
-            if cmd[1] == '1':
-                if maxq:
-                    v=-maxq[0]
-                    if pd[v] > 1:
-                        pd[v] -= 1
-                    else:
-                        heappop(maxq)
-                        pd.pop(v)
-                else:
-                    minq.sort()
-                    v=minq[-1]
-                    if pd[v] > 1:
-                        pd[v] -= 1
-                    else:
-                        pd.pop(v)
-                        minq.pop()
-            else:
-                if minq:
-                    v=minq[0]
-                    if pd[v] > 1:
-                        pd[v] -= 1
-                    else:
-                        heappop(minq)
-                        pd.pop(v)
-                else:
-                    maxq.sort()
-                    v=-maxq[-1]
-                    if pd[v] > 1:
-                        pd[v] -= 1
-                    else:
-                        pd.pop(v)
-                        maxq.pop()
-    if minq and maxq:
-        print(-maxq[0], minq[0])
-    elif minq:
-        minq.sort()
-        print(minq[-1], minq[0])
-    elif maxq:
-        maxq.sort()
-        print(-maxq[0], -maxq[-1])
-    else:
-        print("EMPTY")
-
-
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        solve()
+print(adj_lst)
